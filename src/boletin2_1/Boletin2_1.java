74ae343135a41b7ef1b1d4bcbf8377ee3616a508
asotodominguez <asotodominguez@victoria16.danielcastelao.org>
2017-10-09 08:49:47 +0200

    Inicio proyecto

---
+++ src/boletin2_1/Boletin2_1.java
@@ -0,0 +1,12 @@
+
+package boletin2_1;
+
+public class Boletin2_1 {
+
+    public static void main(String[] args) {
+        float base = 4f; float altura = 3f, area;
+        area = base*altura/2;
+        System.out.println("area = "+area);
+    }
+    
+}
